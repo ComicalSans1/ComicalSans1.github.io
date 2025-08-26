5b0551dc31d787a7236ac860329d4bda0ca2d840
ComicalSans1 <hewhototallymustnotbenamed@gmail.com>
2025-08-26 14:38:36 +0530

    Quartz sync: Aug 26, 2025, 2:38 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,47 +1,135 @@
+// import { PageLayout, SharedLayout } from "./quartz/cfg"
+// import * as Component from "./quartz/components"
+
+// // components shared across all pages
+// export const sharedPageComponents: SharedLayout = {
+//   head: Component.Head(),
+//   header: [],
+//   afterBody: [],
+//   footer: Component.Footer({
+//     links: {
+//       GitHub: "https://github.com/jackyzha0/quartz",
+//       "Discord Community": "https://discord.gg/cRFFHYye7t",
+//       "RSS": "/index.xml" // Add this line
+//     },
+//   }),
+// }
+
+// // components for pages that display a single page (e.g. a single note)
+// export const defaultContentPageLayout: PageLayout = {
+//   beforeBody: [
+//     Component.ConditionalRender({
+//       component: Component.Breadcrumbs(),
+//       condition: (page) => page.fileData.slug !== "index",
+//     }),
+//     Component.ArticleTitle(),
+//     Component.ContentMeta(),
+//     Component.TagList(),
+//   ],
+//   left: [
+//     Component.PageTitle(),
+//     Component.MobileOnly(Component.Spacer()),
+//     Component.Flex({
+//       components: [
+//         {
+//           Component: Component.Search(),
+//           grow: true,
+//         },
+//         { Component: Component.Darkmode() },
+//         { Component: Component.ReaderMode() },
+//       ],
+//     }),
+//     Component.Explorer(),
+//   ],
+//   right: [
+//     Component.Graph(),
+//     Component.DesktopOnly(Component.TableOfContents()),
+//     Component.Backlinks(),
+//   ],
+// }
+
+// // components for pages that display lists of pages  (e.g. tags or folders)
+// export const defaultListPageLayout: PageLayout = {
+//   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+//   left: [
+//     Component.PageTitle(),
+//     Component.MobileOnly(Component.Spacer()),
+//     Component.Flex({
+//       components: [
+//         {
+//           Component: Component.Search(),
+//           grow: true,
+//         },
+//         { Component: Component.Darkmode() },
+//       ],
+//     }),
+//     Component.Explorer(),
+//   ],
+//   right: [],
+// }
+
+
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
+
+const recentNotes = [
+  Component.RecentNotes({
+    title: "Recent Writing",
+    limit: 4,
+    filter: (f) =>
+      f.slug!.startsWith("Posts/") && f.slug! !== "Posts/index" && !f.frontmatter?.noindex,
+    linkToMore: "posts/" as SimpleSlug,
+  }),
+  Component.RecentNotes({
+    title: "Recent Notes",
+    limit: 2,
+    filter: (f) => f.slug!.startsWith("thoughts/"),
+    linkToMore: "thoughts/" as SimpleSlug,
+  }),
+]
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [...recentNotes.map((c) => Component.MobileOnly(c))],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/jackyzha0",
+      Twitter: "https://twitter.com/_jzhao",
     },
   }),
 }
 
+const left = [
+  Component.PageTitle(),
+  Component.MobileOnly(Component.Spacer()),
+  Component.Flex({
+    components: [
+      {
+        Component: Component.Search(),
+        grow: true,
+      },
+      { Component: Component.Darkmode() },
+    ],
+  }),
+  ...recentNotes.map((c) => Component.DesktopOnly(c)),
+]
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta(), Component.TagList()],
+  left,
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        showTags: false,
+      },
+      globalGraph: {
+        showTags: false,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
@@ -49,20 +137,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()],
+  left,
   right: [],
 }
